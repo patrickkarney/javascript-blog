b3748bad868e0095d754672e4325dab72600c11f
Patrick Karney <patrickkarney@gmail.com>
2020-12-10 15:48:56 +0100

    Add active class to links

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
 {
-   /* document.getElementById('test-button').addEventListener('click', function(){
+    document.getElementById('test-button').addEventListener('click', function(){
         const links = document.querySelectorAll('.titles a');
         console.log('links:', links);
-  });*/
+  });
 
   const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event);
+    const clickedElement = this;
   
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
-      activeLink.classList.remove('active');
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+  /* [DONE] remove class 'active' from all articles */
+
   const activeArticles = document.querySelectorAll('.posts .active');
 
-    for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
   }
 
   /* get 'href' attribute from the clicked link */
