00865dc928127d510f1d67210b8a8e0d04797011
Patrick Karney <patrickkarney@gmail.com>
2021-01-02 10:51:04 +0100

    Add TagList to right column

--- js/script.js
+++ js/script.js
@@ -41,7 +41,8 @@
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optArticleAuthorsSelector = '.post-author';
+  optArticleAuthorsSelector = '.post-author',
+  optTagsListSelector = '.post .list';
 
   function generateTitleLinks(customSelector = ''){
     
@@ -70,6 +71,8 @@
   generateTitleLinks();
 
   function generateTags(){
+    /* [NEW] create a new variable allTags with an empty array */
+    let allTags = [];
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
     /* START LOOP: for every article: */
@@ -91,12 +94,22 @@
         
         /* add generated code to html variable */
         html = html + linkHTML;
+        /* [NEW] check if this link is NOT already in allTags */
+        if(allTags.indexOf(linkHTML) == -1){
+          /* [NEW] add generated code to allTags array */
+          allTags.push(linkHTML);
+        }
       /* END LOOP: for each tag */
       }
-      /* insert HTML of all the links into the tags wrapper */
-      tagWrapper.innerHTML = html;
+    /* insert HTML of all the links into the tags wrapper */
+    tagWrapper.innerHTML = html;
     /* END LOOP: for every article: */
     }
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+    /* [NEW] add html from allTags to tagList */
+    tagList.innerHTML = allTags.join(' ');
+    
   }
   
   
